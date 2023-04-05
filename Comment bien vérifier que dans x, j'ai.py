0c5b4e3436180c9c993586340e2fc1b92b265804
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2023-04-05 15:15:15 +0200

    Add files via upload

--- Comment bien vérifier que dans x, j'ai.py
+++ Comment bien vérifier que dans x, j'ai.py
@@ -6,25 +6,25 @@ x = '3*3'*3**2
 i = 0
 count = 0
 new_count = 0
-CONSIGNE = x[i:i+3] == '3*3'
 
-while CONSIGNE:
+if x[i:i+3] == '3*3':
     count += 1
-    i = i + 1
-if not CONSIGNE:
+    i = i + 1 # -- j'incrémente ma variable i pour parcourir la liste 
+    # (sans oublier i + 1 pour trigger les fanas algorithmicien)--
+print("Oui, il y a bien {} fois une itération de {}".format(count, x[0:3]))
+
+if not x[i:i+3] == '3*3':
     count += 1
     print("Hélas, pas d'itération de ce genre")
 
     # -- jusque là, ce que j'ai fait est qu'il parcourt la séquence de x qui est
     # censée être de même longueur que '3*3' et examine si cette séquence correspond à
     # '3*3' --
+    
+   
+    
 
-
-
-    # -- j'incrémente ma variable i pour parcourir la liste 
-    # (sans oublier i + 1 pour trigger les fanas algorithmicien)--
-
-print("Oui, il y a bien {} fois une itération de {}".format(count, x[0:3]))    
+ 
 
 
 """en faire une fonction pour une iteration de a dans b quelconque"""
