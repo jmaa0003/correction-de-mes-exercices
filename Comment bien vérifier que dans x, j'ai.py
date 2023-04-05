ad5a25b69cec031882f83c0b509fdbbb4c2f5126
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2023-04-05 15:45:56 +0200

    Add files via upload
    j'ai utilisé cette fois-ci range(), je suis certain que cela m'amène plus près de la réponse à cette question

--- Comment bien vérifier que dans x, j'ai.py
+++ Comment bien vérifier que dans x, j'ai.py
@@ -5,22 +5,21 @@ de fois je l'ai ?"""
 x = '3*3'*3**2
 i = 0
 count = 0
-new_count = 0
 
-if x[i:i+3] == '3*3':
-    count += 1
-    i = i + 2 # -- j'incrémente ma variable i pour parcourir la liste 
-    # (sans oublier i + 1 pour trigger les fanas algorithmicien)--
-    print("Oui, il y a bien {} fois une itération de {}".format(count, x[0:3]))
-
-if not x[i:i+3] == '3*3':
-    count += 1
-    print("Hélas, pas d'itération de ce genre")
+for iteration in range(len(x)//3-1):
+    if x[i:i+3] == '3*3':
+        count += 1
+        i = i + 2 # -- j'incrémente ma variable i pour parcourir la liste 
+        # (sans oublier i + 1 pour trigger les fanas algorithmicien)--
+        
+    else:
+        print("Hélas, pas d'itération de ce genre")
 
     # -- jusque là, ce que j'ai fait est qu'il parcourt la séquence de x qui est
     # censée être de même longueur que '3*3' et examine si cette séquence correspond à
     # '3*3' --
     
+print("Oui, il y a bien {} fois une itération de {}".format(count, x[0:3]))   
    
     
 
