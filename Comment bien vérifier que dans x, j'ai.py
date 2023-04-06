c32b3b9c6a4038d195bc4f3cb775f4a9c3d6e512
jmaa0003 <119824296+jmaa0003@users.noreply.github.com>
2023-04-06 13:34:14 +0200

    Add files via upload
    je devait incrémenter i à +3 et non 2 comme ça cela prend la séquence 0:0+3 puis 3:3+3 ...

--- Comment bien vérifier que dans x, j'ai.py
+++ Comment bien vérifier que dans x, j'ai.py
@@ -9,17 +9,16 @@ count = 0
 for iteration in range(len(x)//3-1):
     if x[i:i+3] == '3*3':
         count += 1
-        i = i + 2 # -- j'incrémente ma variable i pour parcourir la liste 
+        i = i + 3 # -- j'incrémente ma variable i pour parcourir la liste 
         # (sans oublier i + 1 pour trigger les fanas algorithmicien)--
-        
-    else:
-        print("Hélas, pas d'itération de ce genre")
 
     # -- jusque là, ce que j'ai fait est qu'il parcourt la séquence de x qui est
     # censée être de même longueur que '3*3' et examine si cette séquence correspond à
     # '3*3' --
-    
-print("Oui, il y a bien {} fois une itération de {}".format(count, x[0:3]))   
+if count > 0:  
+    print("Oui, il y a bien {} fois une itération de {}".format(count, x[0:3]))
+else:
+    print("Hélas, pas d'itération de ce genre")
    
     
 
